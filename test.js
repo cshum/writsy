76abcd8142263e792c31f866b028734e68a3c97b
Adrian Shum <adrianshum@ecargo.com>
2017-03-07 14:20:45 +0800

    add error cases

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var from = require('from2')
 var concat = require('concat-stream')
 var pump = require('pump')
 
-test('wrap writestream and flush success', (t) => {
+test('writify flush success', (t) => {
   t.plan(4)
   var flushed = false
   var writer = writify.obj((cb) => {
@@ -25,5 +25,39 @@ test('wrap writestream and flush success', (t) => {
   pump(from.obj([1, 2, 3]), writer, (err) => {
     t.error(err)
     t.ok(flushed, 'flushed')
+  })
+})
+
+test('writify flush error', (t) => {
+  t.plan(3)
+  var inner
+  var writer = writify.obj((cb) => {
+    process.nextTick(() => {
+      inner = concat({encoding: 'objects'}, (arr) => {
+        t.deepEqual(arr, [1, 2, 3])
+      })
+      cb(null, inner)
+    })
+  }, (cb) => {
+    process.nextTick(() => {
+      cb(new Error('flush error'))
+    })
+  })
+  pump(from.obj([1, 2, 3]), writer, (err) => {
+    t.equal(err.message, 'flush error')
+    t.ok(writer.destroyed, 'writer destroyed')
+  })
+})
+
+test('wrifity init error', (t) => {
+  t.plan(2)
+  var writer = writify.obj((cb) => {
+    process.nextTick(() => cb(new Error('init error')))
+  }, (cb) => {
+    t.fail('should not flush on init error')
+  })
+  pump(from.obj([1, 2, 3]), writer, (err) => {
+    t.equal(err.message, 'init error')
+    t.ok(writer.destroyed, 'stream destroyed')
   })
 })
